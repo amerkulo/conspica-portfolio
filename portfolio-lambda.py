67c537c9371bed0073c95abfc9afb42e4a071bb4
Alex Merkulov <merkulov.alex@gmail.com>
2018-03-21 17:02:47 -0400

    Added lambda function and zip with python code.

--- portfolio-lambda.py
+++ portfolio-lambda.py
@@ -4,17 +4,29 @@ import StringIO
 import zipfile
 from botocore.client import Config
 import mimetypes
-#s3 = boto3.resource('s3')
-s3 = boto3.resource('s3',config=Config(signature_version='s3v4'))
-pb = s3.Bucket('conspica.com')
-pbstagin = s3.Bucket('staging.conspica.com')
-Memfile = StringIO.StringIO()
-pbstagin.download_fileobj('conspica-portfolio-build.zip',Memfile)
-with zipfile.ZipFile(Memfile) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        pb.upload_fileobj(obj,nm,ExtraArgs={'ContentType':mimetypes.guess_type(nm)[0]})
-        pb.Object(nm).Acl().put(ACL='public-read')
+
+def lambda_handler(event, context):
+   #s3 = boto3.resource('s3')
+   sns = boto3.resource('sns')
+   topic = sns.Topic('arn:aws:sns:us-east-1:659649740988:conspica-portfolio-build')
+   try:
+        s3 = boto3.resource('s3',config=Config(signature_version='s3v4'))
+        pb = s3.Bucket('conspica.com')
+        pbstagin = s3.Bucket('staging.conspica.com')
+        Memfile = StringIO.StringIO()
+        pbstagin.download_fileobj('conspica-portfolio-build.zip',Memfile)
+        with zipfile.ZipFile(Memfile) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                pb.upload_fileobj(obj,nm,ExtraArgs={'ContentType':mimetypes.guess_type(nm)[0]})
+                pb.Object(nm).Acl().put(ACL='public-read')
+
+        print "Code build sucess."
+        topic.publish( Subject="Code delpoy Sucess", Message="Conspica portfolio code was successfully delpoyed")
+   except:
+       print "Colde build failed."
+       topic.publish( Subject="Code delpoy failed", Message="Conspica portfolio code failed to deploy.")
+       raise
 
 
 """
